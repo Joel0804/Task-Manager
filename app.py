3837fd0a7b3f58f6920a052d890e2c70adc1fa4f
Joel Dsilva <joeldsilva0804@gmail.com>
2026-04-16 13:49:07 +0530

    authentication

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 import hashlib
 DB_PATH = "database.db" # this is file name
 
@@ -37,25 +37,64 @@ app.secret_key = 'devkey'
 @app.route("/register", methods=["POST"]) # post to submit
 def register(): 
     data = request.get_json()# gets data in json format it converts into dictionary
-    print(data)
     username = data["username"] 
     password = data["password"]
     
     pw_hash = hashlib.sha256(password.encode()).hexdigest() 
-    
-# .encode() — converts the string to bytes (sha256 needs bytes, not a string)
-#hashlib.sha256(...) — runs the hashing algorithm
-#.hexdigest() — converts the result to a readable string
-
+    # .encode() — converts the string to bytes (sha256 needs bytes, not a string)
+    #hashlib.sha256(...) — runs the hashing algorithm 
+    #.hexdigest() — converts the result to a readable string
     conn = get_db()
-    conn.execute(
+    try:
+        conn.execute(
         """INSERT INTO users(username, pw_hash) VALUES (?, ?)""" , 
         (username, pw_hash)
-    )
-    conn.commit()
-    conn.close()
-    return jsonify({"message": "Account created!"})
+        )
+        conn.commit()
+        jsonify({"message": "Account created!"})
+    except sqlite3.IntegrityError:
+        return jsonify({"error": "Username already taken"}), 409
+    finally:
+        conn.close()
+        
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data["username"]
+    password = data["password"]
+    
+    pw_hash = hashlib.sha256(password.encode()).hexdigest()
 
+    conn = get_db()
+    try:
+        user = conn.execute("SELECT * FROM users WHERE username = ?", 
+                     (username, )).fetchone()
+    finally:
+        conn.close()
+    if user is None:
+         return jsonify({"error": "User not found"}), 401
+    
+    if user["pw_hash"] != pw_hash:
+        return jsonify({"error": "Wrong password"}), 401
+    
+    session["user_id"] = user["id"]
+    session["username"] = user["username"]
+
+    return jsonify({"message": f"Welcome {username}!"})
+
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    session.clear()
+    return jsonify({"message": "Logged out"})
+  
+   
+@app.route('/me', methods=["GET"])
+def me():
+    if "user_id" not in session:
+        return jsonify({"error": "Not logged in"}), 401
+    return jsonify({"username": session["username"]})
+    
 
 if __name__ == "__main__":
     init_db()
